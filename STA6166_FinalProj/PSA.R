36cc6e58dcf28b91cd36f7f042828a9ffcaffc98
sayakbiswas <sayak90@gmail.com>
2017-04-22 16:38:53 -0400

    Added a partial quadratic model

--- STA6166_FinalProj/PSA.R
+++ STA6166_FinalProj/PSA.R
@@ -13,9 +13,22 @@ plot(psa_dat$PSA_lvl~psa_dat$CP, xlab='CP', ylab='PSA Level', pch=19)
 plot(psa_dat$PSA_lvl~psa_dat$Gleason_score, xlab='Gleason Score', ylab='PSA Level', pch=19)
 
 #Correlation Matrix
-round(cor(psa_dat[c('ID', 'PSA_lvl', 'Cancer_vol', 'Weight', 'Age', 
+cor_mat = round(cor(psa_dat[c('ID', 'PSA_lvl', 'Cancer_vol', 'Weight', 'Age', 
                     'BPH', 'SVI', 'CP', 'Gleason_score')]), 3)
+View(cor_mat)
 
 source("http://www.stat.ufl.edu/~athienit/check.R")
-reg <- lm(PSA_lvl ~ Cancer_vol + Weight + Age + BPH + SVI + CP, data = psa_dat)
-check(reg, tests=TRUE)
+
+#Full Linear Model
+psa.lm.full <- lm(PSA_lvl ~ Cancer_vol + Weight + Age + BPH + SVI + CP, data = psa_dat)
+check(psa.lm.full, tests=TRUE)
+summary(psa.lm.full)
+plot(psa.lm.full)
+
+#Partial Quadratic Model - Derived using the correlation matrix
+cancervol_svi <- psa_dat$Cancer_vol*psa_dat$SVI
+cancervol_cp <- psa_dat$Cancer_vol*psa_dat$CP
+svi_cp <- psa_dat$SVI*psa_dat$CP
+psa.qm.partial <- lm(PSA_lvl ~ Cancer_vol + Weight + Age + BPH + SVI + CP + cancervol_svi 
+                     + cancervol_cp + svi_cp, data = psa_dat)
+summary(psa.qm.partial)
